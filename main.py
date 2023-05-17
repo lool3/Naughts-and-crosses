eda0cb274eb27c895e3d798ab5f6eb442e9b94c5
Joe <38761945+lool3@users.noreply.github.com>
2023-05-17 21:01:43 +0100

    Update main.py
    Made it actually play

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 grid = [
-  [" ", " ", " "],
-  [" ", " ", " "],
-  [" ", " ", " "]
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""]
 ]
 
 def display_grid():
     for row in grid:
       print("+---+---+---+\n| ", end='')
       for slot in row:
-        print(slot, end=' | ')
+        if slot: # if not empty
+          print(slot, end=' | ')
+        else:
+          print(" ", end=' | ')
       print()
     print("+---+---+---+")
-    
-def get_coords():
+
+def scan_grid(x, y, player):
+  if (grid[-y][0] == grid[-y][1] == grid[-y][2] == player or
+      grid[0][x-1] == grid[1][x-1] == grid[2][x-1] == player or
+      grid[0][0] == grid[1][1] == grid[2][2] == player or
+      grid[0][2] == grid[1][1] == grid[2][0] == player):
+      return f"{player} wins!"
+  elif 1 == 2:
+    """
+    TODO Make something to detect a draw
+    """
+    pass
+  else:
+    return None
+
+def play(player):
     coords = {
       "X":0,
       "Y":0
@@ -29,13 +46,26 @@ def get_coords():
             continue
         except ValueError:
           print("That's not an integer!")
-    return coords
-    
-def plot_coords():
+          
     x, y = coords.values()
-    grid[-y][x-1] = "X" # TODO change X to 'X' or 'O' depending on who's turn
+    while True:
+      if not grid[-y][x-1]: # Make sure empty
+        grid[-y][x-1] = player
+        break
+      else:
+        print("Sorry, you can't play there.")
+        
+    check = scan_grid(x, y, "X" if player else "O")
+    if check: # Check for winner / draw
+      print(check)
+      exit(1)
     
-display_grid()
-coords = get_coords()
-plot_coords()
-display_grid()
+def main():
+    player = True # = X
+    while True:
+      display_grid()
+      play("X" if player else "O")
+      player = not player
+
+if __name__ == "__main__":
+  main()
